9fb8b9eeca850a5288b39c6699a04001d83db6d0
taniacruzz <tanicruz112@gmail.com>
2023-03-17 20:00:58 -0300

    aula 17_03

---
+++ Familia.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+public class Familia{
+   
+    int nDeFamiliares;
+    List<Filho> filhos;
+    Chefe chefeDeFamilia;
+    boolean patriarcal;
+    List<Parente> parentes;
+
+    // public Familia(){
+    //     this.parentes = 
+    // }
+    
+    
+
+    public Parente decidirAlvoDaFofoca(){
+        Random random = new Random();
+        int randomIndex = random.nextInt(parentes.size());
+        return parentes.get(randomIndex);
+        
+    }
+
+    public static void fofocar(Parente parente){
+
+    }
+        
+
+}
+
