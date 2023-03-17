f53317eb8d14992849092e763ef6566cbf190b4b
taniacruzz <tanicruz112@gmail.com>
2023-03-17 20:22:41 -0300

    aula 17_03

--- Familia.java
+++ Familia.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 public class Familia{
    
     int nDeFamiliares;
--- Filho.java
+++ Filho.java
@@ -1,3 +1,14 @@
 public class Filho {
+    String nome;
+    Integer idade;
+
+     public static void atormentarOsPais(){
+
+     }
+
+     public static void fazerAlgoQuePreste(){
+        
+     }
+
     
 }
